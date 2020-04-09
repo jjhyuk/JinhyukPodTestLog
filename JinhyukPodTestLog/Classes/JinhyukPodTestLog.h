cd73e567b93fb9ab6ba1f26b7090516d789ab3f9
jinhyuk <jjhyuk15@gmail.com>
2020-04-09 17:49:19 +0900

    JinhyukPodTestLog TestProject

---
+++ JinhyukPodTestLog/Classes/JinhyukPodTestLog.h
@@ -0,0 +1,16 @@
+//
+//  JinhyukPodTestLog.h
+//  JinhyukPodTestLog
+//
+//  Created by jinhyuk on 2020/04/09.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JinhyukPodTestLog : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
